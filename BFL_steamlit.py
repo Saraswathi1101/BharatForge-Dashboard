a4619b04745ab5a7ed0f96021266c6d19e5d6c8d
Saraswathi1101 <159103879+Saraswathi1101@users.noreply.github.com>
2024-08-01 15:43:28 +0530

    Update BFL_steamlit.py

--- BFL_steamlit.py
+++ BFL_steamlit.py
@@ -8,9 +8,12 @@ st.set_page_config(
 # URL for the Power BI report
 powerbi_url = "https://app.fabric.microsoft.com/view?r=eyJrIjoiODNlNjY0ZTctOTllNi00YjA3LTljYWQtMmNmM2M2MzJkNjZkIiwidCI6ImE2YjQ2MzBjLTIwMjktNDk2OC1iMjMxLTRkODM1NmNhODFkMCJ9"
 
+iframe_width = "1200px"  # Adjust to fit your screen
+iframe_height = "800px"  # Adjust to fit your screen
+
 iframe_code = f'''
-<div style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; margin: 0; padding: 0; box-sizing: border-box;">
-    <iframe src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="width: 100vw; height: 100vh; border: none; margin: 0; padding: 0;"></iframe>
+<div style="position: absolute; top: 0; left: 0; width: {iframe_width}; height: {iframe_height}; margin: 0; padding: 0; box-sizing: border-box;">
+    <iframe src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="width: 100%; height: 100%; border: none; margin: 0; padding: 0;"></iframe>
 </div>
 '''
 
