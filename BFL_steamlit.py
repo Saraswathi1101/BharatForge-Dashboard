932a5ee86127beec8c37b266146f41b3779c50e4
Saraswathi1101 <159103879+Saraswathi1101@users.noreply.github.com>
2024-08-01 16:20:09 +0530

    Update BFL_steamlit.py

--- BFL_steamlit.py
+++ BFL_steamlit.py
@@ -8,17 +8,15 @@ st.set_page_config(
 # URL for the Power BI report
 powerbi_url = "https://app.fabric.microsoft.com/view?r=eyJrIjoiODNlNjY0ZTctOTllNi00YjA3LTljYWQtMmNmM2M2MzJkNjZkIiwidCI6ImE2YjQ2MzBjLTIwMjktNDk2OC1iMjMxLTRkODM1NmNhODFkMCJ9"
 
-# iframe_width = "1380px"  # Adjust to fit your screen
-# iframe_height = "800px"  # Adjust to fit your screen
 
 # iframe_code = f'''
-# <div style="position: absolute; top: 0; left: 0; width: {iframe_width}; height: {iframe_height}; margin: 0; padding: 0; box-sizing: border-box;">
-#     <iframe src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="width: 100%; height: 100%; border: none; margin: 0; padding: 0;"></iframe>
+# <div style="width: 1390px; box-sizing: border-box; margin-left: -340px; margin-top: -100px; background-color: white;">
+#     <iframe width="100%" height="690" src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="margin-top: 62px; background-color: white;"></iframe>
 # </div>
 # '''
 iframe_code = f'''
-<div style="width: 1390px; box-sizing: border-box; margin-left: -340px; margin-top: -100px; background-color: white;">
-    <iframe width="100%" height="690" src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="margin-top: 62px; background-color: white;"></iframe>
+<div style="width: 1380px; box-sizing: border-box; margin-left: -330px; margin-top: -100px; background-color: white; padding: 0; border: none;">
+    <iframe width="100%" height="680" src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="background-color: white; border: none;"></iframe>
 </div>
 '''
 
