f9048da37ad119fe04ae41d3bb1703342a98edb8
Saraswathi1101 <159103879+Saraswathi1101@users.noreply.github.com>
2024-08-01 16:00:23 +0530

    Update BFL_steamlit.py

--- BFL_steamlit.py
+++ BFL_steamlit.py
@@ -17,9 +17,10 @@ powerbi_url = "https://app.fabric.microsoft.com/view?r=eyJrIjoiODNlNjY0ZTctOTllN
 # </div>
 # '''
 iframe_code = f'''
-<div style="width: 1380px; box-sizing: border-box; margin-left: -330px; margin-top: -100px; ">
-    <iframe width="100%" height="680" src="{powerbi_url}" frameborder="0" allowFullScreen="true" style=" margin-top: 62px;"></iframe>
+<div style="width: 1380px; box-sizing: border-box; margin-left: -330px; margin-top: -100px; background-color: white;">
+    <iframe width="100%" height="680" src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="margin-top: 62px; background-color: white;"></iframe>
 </div>
 '''
+
 # Inject the iframe_code into the Streamlit app
 st.markdown(iframe_code, unsafe_allow_html=True)
