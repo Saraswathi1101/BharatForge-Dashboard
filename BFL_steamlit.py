553a5d4f721e008e22e6d25a221aa68eaf081fdc
Saraswathi1101 <159103879+Saraswathi1101@users.noreply.github.com>
2024-08-01 15:31:26 +0530

    Update BFL_steamlit.py

--- BFL_steamlit.py
+++ BFL_steamlit.py
@@ -9,7 +9,7 @@ st.set_page_config(
 powerbi_url = "https://app.fabric.microsoft.com/view?r=eyJrIjoiODNlNjY0ZTctOTllNi00YjA3LTljYWQtMmNmM2M2MzJkNjZkIiwidCI6ImE2YjQ2MzBjLTIwMjktNDk2OC1iMjMxLTRkODM1NmNhODFkMCJ9"
 
 iframe_code = f'''
-<div style="background-color: white; width: 1370px; box-sizing: border-box; margin-left: -320px; margin-top: -100px; padding: 20px;">
+<div style="background-color: white; width: 1390px; box-sizing: border-box; margin-left: -320px; margin-top: -90px; padding: 20px;">
     <div style="background-color: white;">
         <iframe width="100%" height="1600" src="{powerbi_url}" frameborder="0" allowFullScreen="true" style="margin-top: 55px;"></iframe>
     </div>
